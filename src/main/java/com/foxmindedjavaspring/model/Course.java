f90fcb45a9dfb513b9d0e24a61954474ca6e937b
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-23 13:22:19 +0300

    Course added

---
+++ src/main/java/com/foxmindedjavaspring/model/Course.java
@@ -0,0 +1,56 @@
+package com.foxmindedjavaspring.model;
+
+import javax.security.auth.Subject;
+
+public class Course {
+    private final String topic;
+    private final Subject subject;
+    private final String description;
+
+    private Course(Builder builder) {
+        this.topic = builder.topic;
+        this.subject = builder.subject;
+        this.description = builder.description;
+    }
+
+    public static final class Builder {
+        private String topic;
+        private Subject subject;
+        private String description;
+
+        public Builder() {
+        }
+
+        public Builder withTopic(String topic) {
+            this.topic = topic;
+            return this;
+        }
+
+        public Builder withSubject(Subject subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Course build() {
+            return new Course(this);
+        }
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
