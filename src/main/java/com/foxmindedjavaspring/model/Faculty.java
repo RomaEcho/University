f93debc51d2b6f88d8cf39a033a6dc6008b0e84c
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-23 13:35:20 +0300

    Faculty added

---
+++ src/main/java/com/foxmindedjavaspring/model/Faculty.java
@@ -0,0 +1,32 @@
+package com.foxmindedjavaspring.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faculty {
+    private final String department;
+    private final List<Subject> subjects = new ArrayList<>();
+    private String address;
+
+    public Faculty(String department, String address) {
+        this.department = department;
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
