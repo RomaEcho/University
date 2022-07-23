48d8898ac832010ce91d39aaa44d122ce18397a5
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-23 13:48:37 +0300

    Lecture added

---
+++ src/main/java/com/foxmindedjavaspring/model/Lecture.java
@@ -0,0 +1,35 @@
+package com.foxmindedjavaspring.model;
+
+import java.time.LocalDateTime;
+
+public class Lecture {
+    private final CourseEvent courseEvent;
+    private LocalDateTime startTime;
+    private Integer lab;
+
+    public Lecture(CourseEvent courseEvent, LocalDateTime startTime, Integer lab) {
+        this.courseEvent = courseEvent;
+        this.startTime = startTime;
+        this.lab = lab;
+    }
+
+    public CourseEvent getCourseEvent() {
+        return courseEvent;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public Integer getLab() {
+        return lab;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setLab(Integer lab) {
+        this.lab = lab;
+    }
+}
