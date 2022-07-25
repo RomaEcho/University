10f3312cd1f37e5964c1c03fb4b8b9ac0ce339bf
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-25 10:41:16 +0300

    ExamEvent Builder added

--- src/main/java/com/foxmindedjavaspring/university/model/ExamEvent.java
+++ src/main/java/com/foxmindedjavaspring/university/model/ExamEvent.java
@@ -4,15 +4,57 @@ import java.time.LocalDate;
 
 public class ExamEvent {
     private final Exam exam;
-    private LocalDate date;
-    private ExamState state;
-    private Integer lab;
-    private Integer rate;
+    private final LocalDate date;
+    private final ExamState state;
+    private final Integer lab;
+    private final Integer rate;
 
-    public ExamEvent(Exam exam, LocalDate date, Integer lab) {
-        this.exam = exam;
-        this.date = date;
-        this.lab = lab;
+    private ExamEvent(Builder builder) {
+        this.exam = builder.exam;
+        this.date = builder.date;
+        this.state = builder.state;
+        this.lab = builder.lab;
+        this.rate = builder.rate;
+    }
+
+    public static final class Builder {
+        private Exam exam;
+        private LocalDate date;
+        private ExamState state;
+        private Integer lab;
+        private Integer rate;
+
+        public Builder() {
+        }
+
+        public Builder withExam(Exam exam) {
+            this.exam = exam;
+            return this;
+        }
+
+        public Builder withDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder withState(ExamState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder withLab(Integer lab) {
+            this.lab = lab;
+            return this;
+        }
+
+        public Builder withRate(Integer rate) {
+            this.rate = rate;
+            return this;
+        }
+
+        public ExamEvent build() {
+            return new ExamEvent(this);
+        }
     }
 
     public Exam getExam() {
@@ -23,7 +65,7 @@ public class ExamEvent {
         return date;
     }
 
-    public ExamState getStatus() {
+    public ExamState getState() {
         return state;
     }
 
@@ -35,19 +77,4 @@ public class ExamEvent {
         return rate;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public void setStatus(ExamState status) {
-        this.state = status;
-    }
-
-    public void setLab(Integer lab) {
-        this.lab = lab;
-    }
-
-    public void setRate(Integer rate) {
-        this.rate = rate;
-    }
 }
