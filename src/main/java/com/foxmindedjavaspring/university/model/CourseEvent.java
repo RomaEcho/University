fe5d40f5dbdb49d6c641f0802dd65028c11ce422
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-25 10:33:55 +0300

    CourseEvent made immutable, Builder added

--- src/main/java/com/foxmindedjavaspring/university/model/CourseEvent.java
+++ src/main/java/com/foxmindedjavaspring/university/model/CourseEvent.java
@@ -5,16 +5,56 @@ import java.time.LocalDate;
 public class CourseEvent {
     private final Course course;
     private final LocalDate startDate;
-    private Lecturer lecturer;
-    private Integer numberOfHours;
-    private Integer rate;
+    private final Lecturer lecturer;
+    private final Integer numberOfHours;
+    private final Integer rate;
 
-    public CourseEvent(Course course, LocalDate startDate, Lecturer lecturer,
-            Integer numberOfHours) {
-        this.course = course;
-        this.startDate = startDate;
-        this.lecturer = lecturer;
-        this.numberOfHours = numberOfHours;
+    private CourseEvent(Builder builder) {
+        this.course = builder.course;
+        this.startDate = builder.startDate;
+        this.lecturer = builder.lecturer;
+        this.numberOfHours = builder.numberOfHours;
+        this.rate = builder.rate;
+    }
+
+    public static final class Builder {
+        private Course course;
+        private LocalDate startDate;
+        private Lecturer lecturer;
+        private Integer numberOfHours;
+        private Integer rate;
+
+        public Builder() {
+        }
+
+        public Builder withCourse(Course course) {
+            this.course = course;
+            return this;
+        }
+
+        public Builder withStartDate(LocalDate startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Builder withLecturer(Lecturer lecturer) {
+            this.lecturer = lecturer;
+            return this;
+        }
+
+        public Builder withNumberOfHours(Integer numberOfHours) {
+            this.numberOfHours = numberOfHours;
+            return this;
+        }
+
+        public Builder withRate(Integer rate) {
+            this.rate = rate;
+            return this;
+        }
+
+        public CourseEvent build() {
+            return new CourseEvent(this);
+        }
     }
 
     public Course getCourse() {
@@ -37,15 +77,4 @@ public class CourseEvent {
         return rate;
     }
 
-    public void setLecturer(Lecturer lecturer) {
-        this.lecturer = lecturer;
-    }
-
-    public void setNumberOfHours(Integer numberOfHours) {
-        this.numberOfHours = numberOfHours;
-    }
-
-    public void setRate(Integer rate) {
-        this.rate = rate;
-    }
 }
