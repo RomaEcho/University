37005b4b531f9f00860ccc8f6e4751b12927b28d
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-07-26 11:01:04 +0300

    Faculty class lost array

--- src/main/java/com/foxmindedjavaspring/university/model/Faculty.java
+++ src/main/java/com/foxmindedjavaspring/university/model/Faculty.java
@@ -1,11 +1,7 @@
 package com.foxmindedjavaspring.university.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Faculty {
     private final String department;
-    private final List<Subject> subjects = new ArrayList<>();
     private final String address;
 
     public Faculty(String department, String address) {
@@ -15,10 +11,6 @@ public class Faculty {
 
     public String getDepartment() {
         return department;
-    }
-
-    public List<Subject> getSubjects() {
-        return subjects;
     }
 
     public String getAddress() {
