b41e8ae55cc612ca7fc7d32c8724a5aa913f5387
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-08-21 15:32:49 +0300

    ExamDaoImpl exception handling implemented

--- src/main/java/com/foxmindedjavaspring/university/dao/impl/ExamDaoImpl.java
+++ src/main/java/com/foxmindedjavaspring/university/dao/impl/ExamDaoImpl.java
@@ -4,13 +4,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.foxmindedjavaspring.university.dao.ExamDao;
+import com.foxmindedjavaspring.university.exception.UniversityDataAcessException;
 import com.foxmindedjavaspring.university.model.Exam;
 import com.foxmindedjavaspring.university.utils.Utils;
 
@@ -20,6 +19,10 @@ public class ExamDaoImpl implements ExamDao<Exam> {
     public static final String DELETE_EXAM = "DELETE FROM exams WHERE id = :id";
     public static final String FIND_BY_ID = "SELECT * FROM exams WHERE id = :id";
     public static final String FIND_ALL = "SELECT * FROM exams";
+    private static final String SQL_CREATE_EXAM_ERROR = " :: Error while creating the exam with title:";
+    private static final String SQL_DELETE_EXAM_ERROR = " :: Error while deleting the exam with id:";
+    private static final String SQL_FIND_EXAM_ERROR = " :: Error while searching the exam with id:";
+    private static final String SQL_FIND_ALL_EXAMS_ERROR = " :: Error while searching all exams.";
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public ExamDaoImpl(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -28,25 +31,45 @@ public class ExamDaoImpl implements ExamDao<Exam> {
 
     @Override
     public int create(Exam exam) {
-        return jdbcTemplate.update(CREATE_EXAM,
-                Utils.getMapSinglePair("title", exam.getTitle()));
+        try {
+	    return jdbcTemplate.update(CREATE_EXAM,
+	            Utils.getMapSinglePair("title", exam.getTitle()));
+	} catch (Exception e) {
+        throw new UniversityDataAcessException(
+            SQL_CREATE_EXAM_ERROR + exam.getTitle(), e);
+	}
     }
 
     @Override
     public int delete(long id) {
-        return jdbcTemplate.update(DELETE_EXAM,
-                Utils.getMapSinglePair("id", id));
+        try {
+	    return jdbcTemplate.update(DELETE_EXAM,
+	            Utils.getMapSinglePair("id", id));
+	} catch (Exception e) {
+	    throw new UniversityDataAcessException(
+                SQL_DELETE_EXAM_ERROR + id, e);
+	}
     }
 
     @Override
     public Exam findById(long id) {
-        return jdbcTemplate.queryForObject(FIND_BY_ID,
-                Utils.getMapSinglePair("id", id), new ExamMapper());
+        try {
+	    return jdbcTemplate.queryForObject(FIND_BY_ID,
+	            Utils.getMapSinglePair("id", id), new ExamMapper());
+	} catch (Exception e) {
+	    throw new UniversityDataAcessException(
+                    SQL_FIND_EXAM_ERROR + id, e);
+	}
     }
 
     @Override
     public List<Exam> findAll() {
-        return jdbcTemplate.query(FIND_ALL, new ExamMapper());
+        try {
+	    return jdbcTemplate.query(FIND_ALL, new ExamMapper());
+	} catch (Exception e) {
+	    throw new UniversityDataAcessException(
+                    SQL_FIND_ALL_EXAMS_ERROR, e);
+	}
     }
 
     class ExamMapper implements RowMapper<Exam> {
