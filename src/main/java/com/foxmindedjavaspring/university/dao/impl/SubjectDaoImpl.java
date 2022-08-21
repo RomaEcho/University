d1c9829d80b2e52cb80a1f3f3ba1aa97860b5326
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-08-21 16:20:09 +0300

    SubjectDaoImpl exception handling implemented

--- src/main/java/com/foxmindedjavaspring/university/dao/impl/SubjectDaoImpl.java
+++ src/main/java/com/foxmindedjavaspring/university/dao/impl/SubjectDaoImpl.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.foxmindedjavaspring.university.dao.SubjectDao;
+import com.foxmindedjavaspring.university.exception.UniversityDataAcessException;
 import com.foxmindedjavaspring.university.model.Subject;
 import com.foxmindedjavaspring.university.utils.Utils;
 
@@ -20,6 +21,10 @@ public class SubjectDaoImpl implements SubjectDao<Subject> {
     public static final String DELETE_SUBJECT = "DELETE FROM subjects WHERE id = :id";
     public static final String FIND_BY_ID = "SELECT * FROM subjects WHERE id = :id";
     public static final String FIND_ALL = "SELECT * FROM subjects";
+    private static final String SQL_CREATE_SUBJECT_ERROR = " :: Error while creating the subject with number:";
+    private static final String SQL_DELETE_SUBJECT_ERROR = " :: Error while deleting the subject with id:";
+    private static final String SQL_FIND_SUBJECT_ERROR = " :: Error while searching the subject with id:";
+    private static final String SQL_FIND_ALL_SUBJECTS_ERROR = " :: Error while searching all subjects.";
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public SubjectDaoImpl(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -28,28 +33,48 @@ public class SubjectDaoImpl implements SubjectDao<Subject> {
 
     @Override
     public int create(Subject subject) {
-        Map<String, Object> namedParameters = new HashMap<>();
-        namedParameters.put("number", subject.getNumber());
-        namedParameters.put("name", subject.getName());
-        namedParameters.put("description", subject.getDescription());
-        return jdbcTemplate.update(CREATE_SUBJECT, namedParameters);
+        try {
+            Map<String, Object> namedParameters = new HashMap<>();
+            namedParameters.put("number", subject.getNumber());
+            namedParameters.put("name", subject.getName());
+            namedParameters.put("description", subject.getDescription());
+            return jdbcTemplate.update(CREATE_SUBJECT, namedParameters);
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_CREATE_SUBJECT_ERROR + subject.getNumber(), e);
+        }
     }
 
     @Override
     public int delete(long id) {
-        return jdbcTemplate.update(DELETE_SUBJECT,
-                Utils.getMapSinglePair("id", id));
+        try {
+            return jdbcTemplate.update(DELETE_SUBJECT,
+                    Utils.getMapSinglePair("id", id));
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_DELETE_SUBJECT_ERROR + id, e);
+        }
     }
 
     @Override
     public Subject findById(long id) {
-        return jdbcTemplate.queryForObject(FIND_BY_ID,
-                Utils.getMapSinglePair("id", id), new SubjectMapper());
+        try {
+            return jdbcTemplate.queryForObject(FIND_BY_ID,
+                    Utils.getMapSinglePair("id", id), new SubjectMapper());
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_FIND_SUBJECT_ERROR + id, e);
+        }
     }
 
     @Override
     public List<Subject> findAll() {
-        return jdbcTemplate.query(FIND_ALL, new SubjectMapper());
+        try {
+            return jdbcTemplate.query(FIND_ALL, new SubjectMapper());
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_FIND_ALL_SUBJECTS_ERROR, e);
+        }
     }
 
     class SubjectMapper implements RowMapper<Subject> {
