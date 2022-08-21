cf6b9f784cd478eea41ad7c2516af508dfe4763a
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-08-21 16:14:15 +0300

    PersonDaoImpl exception handling implemented

--- src/main/java/com/foxmindedjavaspring/university/dao/impl/PersonDaoImpl.java
+++ src/main/java/com/foxmindedjavaspring/university/dao/impl/PersonDaoImpl.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.foxmindedjavaspring.university.dao.PersonDao;
+import com.foxmindedjavaspring.university.exception.UniversityDataAcessException;
 import com.foxmindedjavaspring.university.model.Person;
 import com.foxmindedjavaspring.university.utils.Utils;
 
@@ -20,6 +21,14 @@ public class PersonDaoImpl implements PersonDao<Person> {
 	public static final String DELETE_PERSON = "DELETE FROM persons WHERE id = :id";
 	public static final String FIND_BY_ID = "SELECT * FROM persons WHERE id = :id";
 	public static final String FIND_ALL = "SELECT * FROM persons";
+	private static final String SQL_CREATE_PERSON_ERROR = 
+    " :: Error while creating the person with:";
+    private static final String SQL_DELETE_PERSON_ERROR =  
+    " :: Error while deleting the person with id:";
+    private static final String SQL_FIND_PERSON_ERROR = 
+    " :: Error while searching the person with id:";
+    private static final String SQL_FIND_ALL_PERSONS_ERROR = 
+    " :: Error while searching all persons.";
 	private final NamedParameterJdbcTemplate jdbcTemplate;
 
 	public PersonDaoImpl(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -28,44 +37,71 @@ public class PersonDaoImpl implements PersonDao<Person> {
 
 	@Override
 	public int create(Person person) {
-		Map<String, Object> namedParameters = new HashMap<>();
-		namedParameters.put("first_name", person.getFirstName());
-		namedParameters.put("last_name", person.getLastName());
-		namedParameters.put("birth_day", person.getBirthday());
-		namedParameters.put("gender", person.getGender());
-		namedParameters.put("phon", person.getPhone());
-		namedParameters.put("email", person.getEmail());
-		namedParameters.put("address", person.getAddress());
-		return jdbcTemplate.update(CREATE_PERSON, namedParameters);
+		try {
+			Map<String, Object> namedParameters = new HashMap<>();
+			namedParameters.put("first_name", person.getFirstName());
+			namedParameters.put("last_name", person.getLastName());
+			namedParameters.put("birth_day", person.getBirthday());
+			namedParameters.put("gender", person.getGender());
+			namedParameters.put("phon", person.getPhone());
+			namedParameters.put("email", person.getEmail());
+			namedParameters.put("address", person.getAddress());
+			return jdbcTemplate.update(CREATE_PERSON, namedParameters);
+		} catch (Exception e) {
+			throw new UniversityDataAcessException(String.format(
+				"%s first_name: %s, last_name: %s, address: %s.",
+                    SQL_CREATE_PERSON_ERROR,
+					person.getFirstName(),
+					person.getLastName(),
+					person.getAddress()),
+				 e);
+		}
 	}
 
 	@Override
 	public int delete(long id) {
-		return jdbcTemplate.update(DELETE_PERSON,
-				Utils.getMapSinglePair("id", id));
+		try {
+			return jdbcTemplate.update(DELETE_PERSON,
+					Utils.getMapSinglePair("id", id));
+		} catch (Exception e) {
+			throw new UniversityDataAcessException(
+                    SQL_DELETE_PERSON_ERROR + id, e);
+		}
 	}
 
 	@Override
 	public Person findById(long id) {
-		return jdbcTemplate.queryForObject(FIND_BY_ID,
-				Utils.getMapSinglePair("id", id), new PersonMapper());
+		try {
+			return jdbcTemplate.queryForObject(FIND_BY_ID,
+					Utils.getMapSinglePair("id", id), new PersonMapper());
+		} catch (Exception e) {
+			throw new UniversityDataAcessException(
+                    SQL_FIND_PERSON_ERROR + id, e);
+		}
 	}
 
 	@Override
 	public List<Person> findAll() {
-		return jdbcTemplate.query(FIND_ALL, new PersonMapper());
+		try {
+			return jdbcTemplate.query(FIND_ALL, new PersonMapper());
+		} catch (Exception e) {
+			throw new UniversityDataAcessException(
+                    SQL_FIND_ALL_PERSONS_ERROR, e);
+		}
 	}
 
 	class PersonMapper implements RowMapper<Person> {
 		@Override
 		public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return new Person.Builder().withAddress(rs.getString("address"))
-					.withBirthday(rs.getDate("birth_day").toLocalDate())
-					.withEmail(rs.getString("email"))
-					.withFirstName(rs.getString("first_name"))
-					.withGender(rs.getString("gender"))
-					.withLastName(rs.getString("last_name"))
-					.withPhone(rs.getString("phone")).build();
+			return new Person.Builder()
+							 .withAddress(rs.getString("address"))
+							 .withBirthday(rs.getDate("birth_day").toLocalDate())
+					 		 .withEmail(rs.getString("email"))
+					 		 .withFirstName(rs.getString("first_name"))
+					         .withGender(rs.getString("gender"))
+							 .withLastName(rs.getString("last_name"))
+							 .withPhone(rs.getString("phone"))
+							 .build();
 		}
 	}
 }
