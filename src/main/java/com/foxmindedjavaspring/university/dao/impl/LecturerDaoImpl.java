cfe3a923ebb0207cae9bfc3237c1d864ba37bcc6
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-08-21 16:01:14 +0300

    LecturerDaoImpl exception handling implemented

--- src/main/java/com/foxmindedjavaspring/university/dao/impl/LecturerDaoImpl.java
+++ src/main/java/com/foxmindedjavaspring/university/dao/impl/LecturerDaoImpl.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.foxmindedjavaspring.university.dao.LecturerDao;
+import com.foxmindedjavaspring.university.exception.UniversityDataAcessException;
 import com.foxmindedjavaspring.university.model.Lecturer;
 import com.foxmindedjavaspring.university.utils.Utils;
 
@@ -20,6 +21,10 @@ public class LecturerDaoImpl implements LecturerDao<Lecturer> {
     public static final String DELETE_LECTURER = "DELETE FROM lecturers WHERE id = :id";
     public static final String FIND_BY_ID = "SELECT * FROM lecturers WHERE id = :id";
     public static final String FIND_ALL = "SELECT * FROM lecturers";
+    private static final String SQL_CREATE_LECTURER_ERROR = " :: Error while creating the lecturer with staff_id:";
+    private static final String SQL_DELETE_LECTURER_ERROR = " :: Error while deleting the lecturer with id:";
+    private static final String SQL_FIND_LECTURER_ERROR = " :: Error while searching the lecturer with id:";
+    private static final String SQL_FIND_ALL_LECTURERS_ERROR = " :: Error while searching all lecturers.";
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public LecturerDaoImpl(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -28,27 +33,47 @@ public class LecturerDaoImpl implements LecturerDao<Lecturer> {
 
     @Override
     public int create(Lecturer lecturer) {
-        Map<String, Object> namedParameters = new HashMap<>();
-        namedParameters.put("staff_id", lecturer.getStaffId());
-        namedParameters.put("level", lecturer.getLevel());
-        return jdbcTemplate.update(CREATE_LECTURER, namedParameters);
+        try {
+            Map<String, Object> namedParameters = new HashMap<>();
+            namedParameters.put("staff_id", lecturer.getStaffId());
+            namedParameters.put("level", lecturer.getLevel());
+            return jdbcTemplate.update(CREATE_LECTURER, namedParameters);
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_CREATE_LECTURER_ERROR + lecturer.getStaffId(), e);
+        }
     }
 
     @Override
     public int delete(long id) {
-        return jdbcTemplate.update(DELETE_LECTURER,
-                Utils.getMapSinglePair("id", id));
+        try {
+            return jdbcTemplate.update(DELETE_LECTURER,
+                    Utils.getMapSinglePair("id", id));
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_DELETE_LECTURER_ERROR + id, e);
+        }
     }
 
     @Override
     public Lecturer findById(long id) {
-        return jdbcTemplate.queryForObject(FIND_BY_ID,
-                Utils.getMapSinglePair("id", id), new LecturerMapper());
+        try {
+            return jdbcTemplate.queryForObject(FIND_BY_ID,
+                    Utils.getMapSinglePair("id", id), new LecturerMapper());
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_FIND_LECTURER_ERROR + id, e);
+        }
     }
 
     @Override
     public List<Lecturer> findAll() {
-        return jdbcTemplate.query(FIND_ALL, new LecturerMapper());
+        try {
+            return jdbcTemplate.query(FIND_ALL, new LecturerMapper());
+        } catch (Exception e) {
+            throw new UniversityDataAcessException(
+                    SQL_FIND_ALL_LECTURERS_ERROR, e);
+        }
     }
 
     class LecturerMapper implements RowMapper<Lecturer> {
