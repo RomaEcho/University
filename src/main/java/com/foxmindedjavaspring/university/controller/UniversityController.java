61bd55f57cd6b36e9329370623ccf0b7d5c74d03
Petrushenko Roman <lexxxx.lutor@gmail.com>
2022-09-18 20:05:39 +0300

    - university/about.html template and controller implemented; - pom.xml updated

--- src/main/java/com/foxmindedjavaspring/university/controller/UniversityController.java
+++ src/main/java/com/foxmindedjavaspring/university/controller/UniversityController.java
@@ -1,21 +1,25 @@
 package com.foxmindedjavaspring.university.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.foxmindedjavaspring.university.model.University;
+import com.foxmindedjavaspring.university.service.UniversityService;
 
 @Controller
 public class UniversityController {
-    // private final UniversityService universityService;
-
-    // public UniversityController(UniversityService universityService) {
-    //     this.universityService = universityService;
-    // }
     
-    @GetMapping("/")
-    public String show() {
-        // Optional<University> university = universityService.getAllUniversities()
-        //         .stream().findFirst();
-        // model.addAttribute("university", university);
-        return "about";
+    private final UniversityService universityService;
+
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+    
+    @GetMapping("/university")
+    public String show(Model model) {
+        University university = universityService.getAllUniversities().get(0);
+        model.addAttribute("university", university);
+        return "university/about";
     }
 }
---
+++ src/test/java/com/foxmindedjavaspring/university/controller/UniversityControllerTest.java
@@ -0,0 +1,44 @@
+package com.foxmindedjavaspring.university.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import com.foxmindedjavaspring.university.model.University;
+import com.foxmindedjavaspring.university.service.UniversityService;
+
+class UniversityControllerTest {
+    private University university;
+    private List<University> universities;
+    @Mock
+    private UniversityService universityService;
+    @Mock
+    private Model model;
+    @InjectMocks
+    private UniversityController universityController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        university = new University("name", "hqLocation");
+        universities = List.of(university);
+    }
+
+    @Test
+    void shouldVerifyControllerUniversityReturnValue() throws Exception { 
+        when(universityService.getAllUniversities()).thenReturn(universities);
+        String expected = "university/about";
+
+        String actual = universityController.show(model);
+
+        assertEquals(expected, actual);
+    }
+}
